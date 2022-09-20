ea7c862694086735d0b43448ff353ed9b1455bfe
tbreslein <tommy.breslein@protonmail.com>
2022-09-20 16:37:21 +0200

    docs: add docstrings to Config

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,23 @@
 use crate::cli::args::Args;
 
+/// Repoteer configuration
 pub struct Config {
+    /// Whether or not to print verbose output
     pub verbose: bool,
 }
 
 impl Config {
+    /// Constructs a new `Config` from an `&Args`
+    ///
+    /// # Arguments
+    ///
+    /// * `args` - a &Args carrying CLI config
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let config = config::Config::new(&args);   
+    /// ```
     pub fn new(args: &Args) -> Self {
         return Config {verbose: args.verbose};
     }
