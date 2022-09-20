5f043dc656de746e827875e180dd37a021df04b3
tbreslein <tommy.breslein@protonmail.com>
2022-09-20 10:32:02 +0200

    feat: add --manifest cli option

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -7,9 +7,15 @@ use super::commands::Commands;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
-    /// Reads from specific config file
+    /// Reads from specific config file.
+    /// Defaults to $XDG_CONFIG_DIR/repoteer/config.toml
     #[clap(short, long, value_parser, value_name = "FILE")]
     pub config: Option<PathBuf>,
+
+    /// Reads from specific manifest file.
+    /// Defaults to $XDG_CONFIG_DIR/repoteer/manifest.toml
+    #[clap(short, long, value_parser, value_name = "FILE")]
+    pub manifest: Option<PathBuf>,
 
     /// Verbose output
     #[clap(short, long, action)]
