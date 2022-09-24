73dbb5b0ad91aee127cc6e3533bc70f24b3a57b2
tbreslein <tommy.breslein@protonmail.com>
2022-09-24 13:09:57 +0200

    feat: worktree functionality

--- src/operations.rs
+++ src/operations.rs
@@ -1,6 +1,6 @@
-use std::{path::Path, process::Output};
+use std::process::Output;
 
-use color_eyre::Result;
+use color_eyre::{eyre::eyre, Result};
 
 use crate::{
     cli::command::Command,
@@ -9,10 +9,10 @@ use crate::{
 
 pub fn run_operations(command: Command, manifest: Manifest) -> () {
     for repo in manifest.repos.iter() {
-        println!("Repo: {:?}", repo.url);
-        println!("   at {:?}", repo.path);
+        println!("Repo:  {}", repo.url);
+        println!("    at {}", repo.path);
         process(match command {
-            Command::Clone => run_clone(&repo, false),
+            Command::Clone => run_clone(&repo),
             Command::Pull => run_pull(&repo),
             Command::Push => run_push(&repo),
             Command::Sync => run_sync(&repo),
@@ -26,9 +26,15 @@ fn process(result: Result<Output>) -> () {
     match result {
         Ok(output) => {
             if output.status.success() {
-                println!("   Success! Output: {:?}", output.stdout);
+                println!(
+                    "    Success! Output: {}",
+                    std::str::from_utf8(&output.stdout).unwrap_or("unknown output")
+                );
             } else {
-                println!("   Failure! Output: {:?}", output.stderr);
+                println!(
+                    "    Failure! Output: {}",
+                    std::str::from_utf8(&output.stderr).unwrap_or("unknown error")
+                );
             }
         }
         Err(report) => {
@@ -38,7 +44,7 @@ fn process(result: Result<Output>) -> () {
 }
 
 fn has_unstaged_changes(path: &str) -> Result<bool> {
-    return Ok(std::process::Command::new("git")
+    return Ok(!std::process::Command::new("git")
         .args(["status", "--porcelain"])
         .current_dir(path)
         .output()?
@@ -47,7 +53,10 @@ fn has_unstaged_changes(path: &str) -> Result<bool> {
 }
 
 fn get_output_lines(output: Output) -> Result<Vec<String>> {
-    return Ok(String::from_utf8(output.stdout)?.lines().map(|x| x.to_string()).collect());
+    return Ok(String::from_utf8(output.stdout)?
+        .lines()
+        .map(|x| x.to_string())
+        .collect());
 }
 
 fn has_worktrees(path: &str) -> Result<bool> {
@@ -56,72 +65,95 @@ fn has_worktrees(path: &str) -> Result<bool> {
         .current_dir(path)
         .output()?;
     let output_lines: Vec<String> = get_output_lines(output)?;
-    return Ok(output_lines.len() == 1 && output_lines[0].contains("(bare)"));
+    return Ok(output_lines.len() >= 1 && output_lines[0].contains("(bare)"));
 }
 
-// WIP:
-// fn get_branches(path: &str) -> Result<Vec<String>> {
-//     let output = std::process::Command::new("git")
-//         .args(["worktree", "list"])
-//         .current_dir(path)
-//         .output()?;
-//     let output_lines: Vec<String> = get_output_lines(output)?;
-//     return Ok(output_lines.iter().skip_while(|line| line.len() > 2).);
-// }
+fn get_branches(path: &str) -> Result<Vec<String>> {
+    let output = std::process::Command::new("git")
+        .arg("branch")
+        .current_dir(path)
+        .output()?;
+    return Ok(get_output_lines(output)?
+        .into_iter()
+        // .filter(|line| line.starts_with("+ ") || line.starts_with("* "))
+        .map(|mut line| line.split_off(2))
+        .collect());
+}
 
-fn run_clone(repo: &Repo, is_syncing: bool) -> Result<Output> {
-    return if Path::new(&repo.path).join(".git").exists() {
-        if !is_syncing {
-            Ok(std::process::Command::new("echo")
-                .arg("Clone already exists")
-                .output()?)
+fn run_clone(repo: &Repo) -> Result<Output> {
+    Ok(std::process::Command::new("git")
+        // this is a bit ugly,  but unfortunately just setting the last arg to an empty string
+        // in the case of passing --bare does not work, because the process still reads it as
+        // an argument and then complains about receiving too many arguments.
+        // basically, if I were to pass ["clone", &repo.url, &repo.path, ""], the command would
+        // be `git clone <url> <dir> ""`, and then it would complain about that last "".
+        .args(if repo.is_bare.is_some() && repo.is_bare.unwrap() {
+            vec!["clone", &repo.url, &repo.path, "--bare"]
         } else {
-            Ok(std::process::Command::new("").output()?)
-        }
-    } else {
-        Ok(std::process::Command::new("git")
-            .args([
-                "clone",
-                &repo.url,
-                &repo.path,
-                if repo.is_bare.is_some() && repo.is_bare.unwrap() {
-                    "--bare"
-                } else {
-                    ""
-                },
-            ])
-            .output()?)
-    };
+            vec!["clone", &repo.url, &repo.path]
+        })
+        .output()?)
+}
+
+fn get_current_branch(path: &str) -> Result<String> {
+    return Ok(String::from_utf8(std::process::Command::new("git").args(["branch", "--show-current"]).current_dir(path).output()?.stdout)?);
 }
 
 fn run_pull(repo: &Repo) -> Result<Output> {
-    let pull = |path, branch| std::process::Command::new("git")
+    // TODO: this and push closure can be abstracted
+    let pull = |path: &str, branch: &str| {
+        Ok(std::process::Command::new("git")
             .args(["pull", "origin", branch])
             .current_dir(path)
-            .output();
-
-    let has_worktrees = has_worktrees(&repo.path)?;
-    return if has_unstaged_changes(&repo.path)? {
-        Ok(std::process::Command::new("echo")
-            .arg("Has unstaged changes! Cancelling pull")
             .output()?)
-    } else {
-        Ok(pull(&repo.path, "master")?)
     };
+    return if has_unstaged_changes(&repo.path)? {
+        Err(eyre!("Repo has unstaged changes on branch {} pull aborted!", get_current_branch(&repo.path)?))
+    } else {
+        run_operation_with_worktrees(&repo, pull, "Pull")
+    }
 }
 
 fn run_push(repo: &Repo) -> Result<Output> {
-    return Ok(std::process::Command::new("git")
-        .args(["push"])
-        .current_dir(&repo.path)
-        .output()?);
+    let push = |path: &str, branch: &str| {
+        return Ok(std::process::Command::new("git")
+            .args(["push", "origin", branch])
+            .current_dir(path)
+            .output()?);
+    };
+    return run_operation_with_worktrees(&repo, push, "Push");
 }
 
 fn run_sync(repo: &Repo) -> Result<Output> {
-    return run_clone(repo, true)
-        .and(run_pull(repo))
-        .and(run_push(repo))
-        .and(Ok(std::process::Command::new("echo")
-            .arg("Sync complete!")
-            .output()?));
+    run_clone(repo)?;
+    run_pull(repo)?;
+    run_push(repo)?;
+    return Ok(std::process::Command::new("echo")
+        .arg("Sync complete!")
+        .output()?);
+}
+
+fn run_operation_with_worktrees<F>(repo: &Repo, f: F, op: &str) -> Result<Output>
+where
+    F: Fn(&str, &str) -> Result<Output>,
+{
+    let branches = get_branches(&repo.path)?;
+    let has_worktrees = has_worktrees(&repo.path)?;
+
+    println!("      running op: {}", op);
+    for branch in branches.into_iter() {
+        println!("        branch: {}", branch);
+        let path = if has_worktrees {
+            format!("{}/{}", &repo.path, &branch)
+        } else {
+            format!("{}", &repo.path)
+        };
+        match f(&path, &branch) {
+            Ok(_) => {},
+            Err(e) => {println!("   Error! Report: {}", e);}
+        };
+    }
+    return Ok(std::process::Command::new("echo")
+        .arg(format!("{} complete!", op))
+        .output()?);
 }
