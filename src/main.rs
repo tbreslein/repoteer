a9d143efdba135fe26de2043d74d520b0b1d1de9
tbreslein <tommy.breslein@protonmail.com>
2022-10-25 20:11:07 +0200

    I have no clue why I never commented the spantrace thing out

--- src/main.rs
+++ src/main.rs
@@ -27,20 +27,20 @@ fn main() -> Result<()> {
     }
 }
 
-#[cfg(feature = "capture-spantrace")]
-fn install_tracing() {
-    use tracing_error::ErrorLayer;
-    use tracing_subscriber::prelude::*;
-    use tracing_subscriber::{fmt, EnvFilter};
-
-    let fmt_layer = fmt::layer().with_target(false);
-    let filter_layer = EnvFilter::try_from_default_env()
-        .or_else(|_| EnvFilter::try_new("info"))
-        .unwrap();
-
-    tracing_subscriber::registry()
-        .with(filter_layer)
-        .with(fmt_layer)
-        .with(ErrorLayer::default())
-        .init();
-}
+// #[cfg(feature = "capture-spantrace")]
+// fn install_tracing() {
+//     use tracing_error::ErrorLayer;
+//     use tracing_subscriber::prelude::*;
+//     use tracing_subscriber::{fmt, EnvFilter};
+//
+//     let fmt_layer = fmt::layer().with_target(false);
+//     let filter_layer = EnvFilter::try_from_default_env()
+//         .or_else(|_| EnvFilter::try_new("info"))
+//         .unwrap();
+//
+//     tracing_subscriber::registry()
+//         .with(filter_layer)
+//         .with(fmt_layer)
+//         .with(ErrorLayer::default())
+//         .init();
+// }
